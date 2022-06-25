2092b5deb36905bbdcb54d634a38c273d397cc3d
L-j-h-c <ckrgkswnsgh@naver.com>
2022-06-25 16:28:47 +0900

    [Feat] 마커 및 카메라 이동 기능 구현

--- SocialLoginWithNaverMap/Presentation/MainMap/MainMapVC.swift
+++ SocialLoginWithNaverMap/Presentation/MainMap/MainMapVC.swift
@@ -5,6 +5,7 @@
 //  Created by Junho Lee on 2022/06/24.
 //
 
+import CoreLocation
 import UIKit
 
 import NMapsMap
@@ -16,9 +17,20 @@ final class MainMapVC: BaseVC, Storyboarded {
     
     // MARK: - Properties
     
+    private var locationManager = CLLocationManager()
+    private var currentLatitude: Double?
+    private var currentLongitude: Double?
+    
     private lazy var naverMapView: NMFMapView = {
         let map = NMFMapView()
         return map
+    }()
+    
+    private lazy var tempMarker: NMFMarker = {
+        let mk = NMFMarker()
+        mk.position = NMGLatLng(lat: 37.5670135, lng: 126.9783740)
+        mk.mapView = naverMapView
+        return mk
     }()
     
     // MARK: - Life Cycles
@@ -27,6 +39,9 @@ final class MainMapVC: BaseVC, Storyboarded {
         super.viewDidLoad()
         
         view.backgroundColor = .orange
+        DispatchQueue.main.asyncAfter(deadline: .now()+3) {
+            self.currentCoordinate()
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -37,6 +52,23 @@ final class MainMapVC: BaseVC, Storyboarded {
     // MARK: - Bind
     
     // MARK: - Custom Methods
+    
+    private func currentCoordinate() {
+        let locationManager = self.locationManager
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+        let coordinates = locationManager.location?.coordinate
+        self.currentLatitude = coordinates?.latitude
+        self.currentLongitude = coordinates?.longitude
+        
+        tempMarker.position = NMGLatLng(lat: currentLatitude ?? 0, lng: currentLongitude ?? 0)
+        naverMapView.moveCamera(
+            NMFCameraUpdate(
+                position: NMFCameraPosition(NMGLatLng(lat: currentLatitude ?? 0, lng: currentLongitude ?? 0), zoom: 12.0)),
+            completion: nil)
+    }
     
     // MARK: - UI & Layout
     
